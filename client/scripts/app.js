4374237a1fe5aa3a434534aca0f8adfd4769bc83
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 15:54:46 -0700

    passed all initial specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,80 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {};
+
+app.send = function(message) {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function() {
+
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: undefined,
+    type: 'GET',
+    // data: JSON.stringify(message),
+    // contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to fetch message');
+    }
+  });
+
+};
+
+app.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+app.addMessage = function(message) {
+  var username = message.username;
+  var text = message.text;
+  var roomName = message.roomname;
+
+  $('#chats').append('<div>' +
+    '<a class="username" href="#">' + username + '</a>' + ': ' +
+    text + ' in ' +roomName + '</div>');
+};
+
+app.addRoom = function(roomName) {
+  $('#roomSelect').append('<div>' + roomName + '</div>')
+};
+
+app.addFriend = function() {
+};
+
+app.handleSubmit = function() {};
+
+
+$(document).ready(function() {
+  $('#chats').on('click', '.username', function(event) {
+    event.preventDefault();
+    app.addFriend();
+  });
+  $('#send .submit').on('submit', function(event) {
+    event.preventDefault();
+    app.handleSubmit();
+  });
+});
+
+
+
 

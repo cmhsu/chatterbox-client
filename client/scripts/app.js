5ed8f48321087a2dbeea577712ba9bf4b6dd1df5
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 20:24:25 -0700

    added rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,6 @@
 // YOUR CODE HERE:
+var rooms = [];
+
 var app = {};
 
 app.init = function() {};
@@ -31,10 +33,40 @@ app.fetch = function() {
     // data: JSON.stringify(message),
     // contentType: 'application/json',
     success: function (data) {
+      rooms = [];
+      $('select').html('');
       console.log('chatterbox: Message received');
-      console.log(data);
+      $('#chats').html('');
       for (var i = 0; i < data.results.length; i++) {
-        if (data.results[i] != undefined) {
+        app.addMessage(data.results[i]);
+      }
+      for (var i = 0; i < rooms.length; i++) {
+        $('select').append('<option>' + rooms[i] + '</option>')
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to fetch message');
+    }
+  });
+
+};
+
+app.fetchRoom = function(roomValue) {
+
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    // data: JSON.stringify(message),
+    // contentType: 'application/json',
+    success: function (data) {
+      rooms = [];
+      console.log('chatterbox: Message received');
+      $('#chats').html('');
+      for (var i = 0; i < data.results.length; i++) {
+        var roomname = escapeHTML(data.results[i].roomname);
+        if (roomname === roomValue) {
           app.addMessage(data.results[i]);
         }
       }
@@ -59,6 +91,9 @@ app.addMessage = function(message) {
   $('#chats').append('<div>' +
     '<a class="username" href="#">' + username + '</a>' + ': ' +
     text + ' in ' +roomName + '</div>');
+  if (rooms.indexOf(roomName) === -1) {
+    rooms.push(roomName);
+  }
 };
 
 app.addRoom = function(roomName) {
@@ -70,13 +105,14 @@ app.addFriend = function() {
 
 app.handleSubmit = function() {
   var message = {};
-  message.username = 'Piccolo';
+  var index = window.location.search.indexOf('=');
+  var username = window.location.search.slice(index + 1);
+  message.username = username;
   message.text = $('#message').val();
   message.roomname = 'cool roomname';
 
   app.send(message);
 };
-
 
 $(document).ready(function() {
   $('#chats').on('click', '.username', function(event) {
@@ -91,10 +127,10 @@ $(document).ready(function() {
     event.preventDefault();
     app.fetch();
   });
+  $('select').on('change', function(event) {
+      app.fetchRoom(this.value);
+  });
 });
-
-setInterval()
-
 
 function escapeHTML(s) { 
   if (s != undefined) {

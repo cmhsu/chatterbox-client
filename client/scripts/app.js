a9b93a4b0f4fb1eb95dd7fece155f0c9620a99c1
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 17:21:25 -0700

    added escape HTML

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,6 +13,7 @@ app.send = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      console.log(data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -25,12 +26,18 @@ app.fetch = function() {
 
     $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: undefined,
+    url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     // data: JSON.stringify(message),
     // contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message received');
+      console.log(data);
+      for (var i = 0; i < data.results.length; i++) {
+        if (data.results[i] != undefined) {
+          app.addMessage(data.results[i]);
+        }
+      }
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -45,9 +52,9 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  var username = message.username;
-  var text = message.text;
-  var roomName = message.roomname;
+  var username = escapeHTML(message.username);
+  var text = escapeHTML(message.text + '');
+  var roomName = escapeHTML(message.roomname);
 
   $('#chats').append('<div>' +
     '<a class="username" href="#">' + username + '</a>' + ': ' +
@@ -61,7 +68,14 @@ app.addRoom = function(roomName) {
 app.addFriend = function() {
 };
 
-app.handleSubmit = function() {};
+app.handleSubmit = function() {
+  var message = {};
+  message.username = 'Piccolo';
+  message.text = $('#message').val();
+  message.roomname = 'cool roomname';
+
+  app.send(message);
+};
 
 
 $(document).ready(function() {
@@ -69,12 +83,27 @@ $(document).ready(function() {
     event.preventDefault();
     app.addFriend();
   });
-  $('#send .submit').on('submit', function(event) {
+  $('.submit').on('click', function(event) {
     event.preventDefault();
     app.handleSubmit();
   });
+  $('#fetch').on('click', function(event) {
+    event.preventDefault();
+    app.fetch();
+  });
 });
 
+setInterval()
+
+
+function escapeHTML(s) { 
+  if (s != undefined) {
+    return s.replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  }
+}
 
 
 
